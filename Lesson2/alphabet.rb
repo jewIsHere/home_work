50476a16d9ace873348ee2b50f489a7bbc093175
jewIsHere <yudin.serhii@yandex.ru>
2020-02-04 20:29:43 +0800

    Last line Alphabet

---
+++ Lesson2/alphabet.rb
@@ -0,0 +1,15 @@
+alphabet = ("а".."я").to_a
+
+alphabet.insert(6, "ё")
+
+puts alphabet
+
+vowel = "аеёиоуыэюя"
+v_hash = Hash.new
+
+alphabet.each.with_index(1) do |v, i|
+  v_hash[v] = i + 1
+  if vowel.include?(v)
+    puts "#{v} - #{i}"
+  end
+end
