0a23ac536370eb2db13caa31e1f68c1327ea294c
jewIsHere <yudin.serhii@yandex.ru>
2020-02-15 19:12:47 +0800

    Change Loop to While in Fibonacci

--- Lesson2/lionardo_fibonacci.rb
+++ Lesson2/lionardo_fibonacci.rb
@@ -1,11 +1,6 @@
 fibo_num = [0, 1]
-
 num = 0
 
-loop do
-  num = fibo_num[-1] + fibo_num[-2]
-  break if num > 100
-  fibo_num << num
-end
+fibo_num << num while (num = fibo_num[-1] + fibo_num[-2]) <= 100
 
 puts fibo_num
